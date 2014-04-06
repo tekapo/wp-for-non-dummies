c19b84075539999c6ce4530b3f67c4311ee6a3c2
JOTAKI Taisuke <tekapo@gmail.com>
2014-04-06 10:39:38 +0900

    ph!

--- conf.py
+++ conf.py
@@ -102,7 +102,7 @@ pygments_style = 'sphinx'
 
 # import sphinx_rtd_theme
 
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
 
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
